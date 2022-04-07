7524104c9bdb25f219ed4ee0e6a52b600e3e7a1f
Rick-Simons1 <ricksimons12345@yahoo.nl>
2022-04-07 15:21:50 +0200

    feat/auth0-login-flow added login page with login and logout flow to auth0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from '@auth0/auth0-angular';
 import { environment as env } from '../environments/environment';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   auth: {
     domain: auth0Env.domain,
     clientId: auth0Env.clientId,
-    redirectUri: window.location.origin,
+    redirectUri: "http://localhost:4200/login",
   },
 };
 
